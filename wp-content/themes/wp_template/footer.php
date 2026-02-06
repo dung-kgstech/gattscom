8761189ff8eec20fbe30c46ece109f79a3d72e7e
dung-kgstech <dung.hyggelig0809@gmail.com>
2026-02-06 16:49:56 +0700

    create new UI

--- wp-content/themes/wp_template/footer.php
+++ wp-content/themes/wp_template/footer.php
@@ -1,5 +1,45 @@
-<footer></footer>
+<footer class="footer">
+    <div class="footer-container">
+
+        <!-- Left: Follow Us -->
+        <div class="footer-follow">
+            <h4>Follow Us</h4>
+            <div class="social-icons">
+                <a href="#" class="facebook">FACEBOOK</a>
+                <a href="#" class="instagram">INSTAGRAM</a>
+            </div>
+        </div>
+
+        <!-- Center: Logo & Menu -->
+        <div class="footer-center">
+            <h2 class="logo">GattsCOM</h2>
+            <ul class="footer-menu">
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">SERVICES</a></li>
+                <li><a href="#">NEW</a></li>
+                <li><a href="#">BLOG</a></li>
+            </ul>
+        </div>
+
+        <!-- Right: Contact -->
+        <div class="footer-contact">
+            <p>開発のご相談・お問い合わせなど、<br>お気軽にお問い合わせください。</p>
+            <p>
+                住所：<br>
+                ホーチミン市トゥードゥック区、<br>
+                ヴァンファン通り、4番地 29号
+            </p>
+            <p>TEL：0123-456-7890</p>
+        </div>
+
+    </div>
+
+    <div class="footer-bottom">
+        Copyright ©GattsCOM All right reserved
+    </div>
+</footer>
 
 <?php wp_footer(); ?>
 </body>
+
 </html>
--- wp-content/themes/wp_template/front-page.php
+++ wp-content/themes/wp_template/front-page.php
@@ -39,7 +39,7 @@
 			<div class="service-img">
 				<img src="<?php echo get_template_directory_uri(); ?>/images/services1.png" alt="about-images">
 			</div>
-			<div class="service-text">
+			<div class="service-text" style="align-content: space-evenly;">
 				<h3>システム開発</h3>
 				<p>システム開発は、お客様のニーズに合わせて最適なソフトウェアやシステムを提供し、業務の効率化と信頼性向上をサポートします。</p>
 			</div>
@@ -49,7 +49,7 @@
 			<div class="service-img">
 				<img src="<?php echo get_template_directory_uri(); ?>/images/services2.png" alt="about-images">
 			</div>
-			<div class="service-text">
+			<div class="service-text" style="align-content: space-evenly;">
 				<h3>ホームページ制作</h3>
 				<p>ホームページ制作は、お客様の情報を効果的に伝える使いやすいウェブサイトを提供します。</p>
 			</div>
@@ -60,7 +60,7 @@
 			<div class="service-img">
 				<img src="<?php echo get_template_directory_uri(); ?>/images/services3.png" alt="about-images">
 			</div>
-			<div class="service-text">
+			<div class="service-text" style="align-content: space-evenly;">
 				<h3>アプリ開発</h3>
 				<p>アプリ開発は、スマートフォンやタブレット向けの使いやすいアプリを設計・開発するプロセスです。私たちはお客様のニーズに合わせ、便利で魅力的なアプリを提供し、日常やビジネスの効率向上をサポートします。</p>
 			</div>
@@ -68,6 +68,58 @@
 
 	</div>
 </section>
+
+<div class="container">
+	<div class="column-list">
+		<div class="column blog">
+			<h2>BLOG</h2>
+
+			<div class="item">
+				<div class="date">2022年1月25日</div>
+				<div class="tags">
+					<span>Laravel</span><span>Docker</span><span>MYSQL</span><span>SSH</span>
+				</div>
+				<p>Dockerで構築するLaravel開発環境（前編）</p>
+			</div>
+
+			<div class="item">
+				<div class="date">2022年11月8日</div>
+				<div class="tags">
+					<span>MAC</span><span>OS</span><span>Update</span><span>SSH</span>
+				</div>
+				<p>macOS Venturaへのアップデートに伴うSSH接続エラーについて</p>
+			</div>
+
+			<div class="item">
+				<div class="date">2022年1月23日</div>
+				<div class="tags">
+					<span>Laravel</span><span>Composer</span><span>NPM</span><span>SSL</span>
+				</div>
+				<p>CentOS Stream9 LAMP構築から色々含めて</p>
+			</div>
+		</div>
+
+		<div class="column news">
+			<h2>NEWS</h2>
+
+			<div class="item">
+				<div class="date">2025年12月8日</div>
+				<p>年末年始休業のお知らせ</p>
+			</div>
+
+			<div class="item">
+				<div class="date">2024年8月2日</div>
+				<p>2024年 夏季休業のお知らせ</p>
+			</div>
+
+			<div class="item">
+				<div class="date">2022年1月1日</div>
+				<p>新年明けましておめでとうございます。</p>
+			</div>
+		</div>
+	</div>
+</div>
+
 
 
 <?php get_footer(); ?>
